821ceef765a914188de5e0e170857c7efd1bf053
manoubiovic <moetez12345@gmail.com>
2022-09-19 10:50:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Listuser from "./Component/Listuser";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title"> list of users</h1>
+      <Listuser />
+    </div>
+  );
+}
+
+export default App;
